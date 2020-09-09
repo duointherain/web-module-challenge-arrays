ce0296222bb939e00baffe48584c43bf16340f7b
duointherain <duointherain@gmail.com>
2020-09-09 15:27:04 -0700

    So much to learn, so far to go

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", 
 function removeLastFlavor(param1){
 
     /*code here*/
+    
     param1.pop();
     return param1;
 }
@@ -124,7 +125,7 @@ For example, removeFlavorByName(originalFlavors, "Vanilla") would return an arra
 Hint: You can use .splice() for this
 
 */
-let update = 0
+
 
 function removeFlavorByName(param1, param2){
     let sliceDice = param1.indexOf('param2');
@@ -145,11 +146,17 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
+function copy(param1, param2){
 
     /*code here*/
+    
+    param2 = param1.slice();
+    
 
 }
+
+console.log(copy(juice, drinks));
+
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -166,9 +173,9 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
+function filterByWord(param1, param2){
 
-    /*code here*/
+   
 
 }
 
