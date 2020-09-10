3e53d24cb59ccafdf8d6f7641f739882ce9b3667
duointherain <duointherain@gmail.com>
2020-09-10 14:21:39 -0700

    I got it!

--- index.js
+++ index.js
@@ -146,18 +146,33 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-let drinks = 0
 
+console.log('***Task 6 ***');
 function copy(param1, param2){
-
-    /*code here*/
     
-    param2 = param1.slice();
+   
+        param2 = [...param1];
+        
+
     return param2;
-
 }
+console.log(copy(juice, 'drinks'));
 
-console.log(copy(juice, drinks));
+
+
+
+//     let newArr = [];
+//     while (num >= 1) {
+//       // change code below this line
+//       newArr = [...arr];
+//       // change code above this line
+//       num--;
+//     }
+//     return newArr;
+//   }
+  
+//   // change code here to test different cases:
+//   console.log(copyMachine([true, false, true], 2));
 
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
@@ -175,6 +190,7 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
+console.log("Task Seven")
 function filterByWord(array1, flavor){
     //console.log(originalFlavors);
     newArray = [];
